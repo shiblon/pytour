ea5eaba41d6e34416f3e986585c053cab4bfaf37
Chris Monson <chris@highentropy.com>
2013-01-23 06:43:23 +0100

    Updated maketar.sh to have more sane behavior.

--- maketar.sh
+++ maketar.sh
@@ -3,5 +3,5 @@
 thisdir=`dirname $0`
 thisversion=`sed -e 's/^VERSION: //p' -e 'd' "$thisdir/CHANGES"`
 outfile="pytour-$thisversion.tar.gz"
-tar --exclude=".hg" --exclude="*.pyc" --exclude="._*" -czf "/tmp/$outfile" "$thisdir"
+tar --exclude="pytour-*.tar.gz" --exclude=".hg" --exclude="*.pyc" --exclude="._*" -czf "/tmp/$outfile" "$thisdir"
 mv "/tmp/$outfile" "$thisdir"
