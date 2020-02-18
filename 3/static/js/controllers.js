1828207432397092b8b191be88de5e67c94c9513
Chris Monson <shiblon@gmail.com>
2020-02-17 23:56:57 -0500

    Add comment explaining exec choice.

--- 3/static/js/controllers.js
+++ 3/static/js/controllers.js
@@ -354,6 +354,12 @@ function CodeCtrl($scope, $http, $location, $timeout) {
           if (code != null && code !== '' && '"\''.includes(code.trim().charAt(0))) {
             code = '__doc__ = ' + code;
           }
+          // Why exec? Because that's by far the easiest way to get all the code
+          // on a single line (so that exception line number reporting works).
+          // We use JSON.stringify because it also happens to play nicely with
+          // just putting quotes around a string and otherwise being
+          // parsimonious with escaping in a way that is Python-compatible, in
+          // our case..
           var prefixedCode = 'exec(' + JSON.stringify($scope._preamble) + ');' + code;
           // TODO: figure out how to set the special var __doc__ properly.
           // TODO: figure out localsid stuff and module meaning.
